11e59e3b4f5a7d9eb9de00446c689a3c01847eb9
Akirahpola <58601635+Akirahpola@users.noreply.github.com>
2020-02-02 23:43:36 +0100

    done

---
+++ src/test/java/com/forcare/demo/SpringBootDemoApplicationTests.java
@@ -0,0 +1,18 @@
+package com.forcare.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.forcare.rest.SpringBootDemoApplication;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = SpringBootDemoApplication.class)
+public class SpringBootDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
